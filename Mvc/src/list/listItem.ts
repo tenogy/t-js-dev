0edf2e983cb978b82313ef588d5f1a86ebd2a9e8
Yuriy Korpachan <korpachan@gmail.com>
2016-06-24 10:31:30 +0300

    typings

---
+++ Mvc/src/list/listItem.ts
@@ -0,0 +1,5 @@
+﻿export class ListItem {
+	constructor(model) {
+		
+	}
+}
---
+++ Mvc/typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/knockout/index.d.ts" />
+/// <reference path="globals/rx/index.d.ts" />
+/// <reference path="globals/spin/index.d.ts" />
